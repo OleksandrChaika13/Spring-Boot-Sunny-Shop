e21f52e2c78aba380e0119ff1d87f43f2abeb4ed
OleksandrChaika13 <axelrod1347@gmail.com>
2023-10-19 00:17:44 +0300

    Add code to "SunnyShopApplication"

--- src/main/java/com/example/app/SpringBootSunnyShop/SunnyShopApplication.java
+++ src/main/java/com/example/app/SpringBootSunnyShop/SunnyShopApplication.java
@@ -1,13 +1,23 @@
 package com.example.app.SpringBootSunnyShop;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class SunnyShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SunnyShopApplication.class, args);
-	}
+	private static final Logger LOGGER =
+			Logger.getLogger(SunnyShopApplication.class.getName());
 
+	public static void main(String[] args) {
+
+		new SpringApplicationBuilder(SunnyShopApplication.class)
+				.bannerMode(Banner.Mode.OFF)
+				.run(args);
+
+		LOGGER.info("APP is running...");
+	}
 }
